8f2d4193ceb3f7c4f485a82426ec0b6e0382cb7e
Nate Couch <ndcouch@gmail.com>
2019-04-11 20:33:08 -0400

    initial code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-//import './App.css';
+import ReactDOM from "react-dom";
+import ChatRoom from "./components/ChatRoom";
+//import * as firebase from "firebase";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        This is the REACT App!
+        <ChatRoom />
       </div>
     );
   }
 }
 
+//ReactDOM.render(<App />, document.getElementById("app")); not sure if needed
 export default App;
